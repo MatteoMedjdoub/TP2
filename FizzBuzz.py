0533d786ba98244346055df60a8d21d0aca3eb98
MEDJDOUB Matteo <12104980@crit-xubuntu>
2023-10-02 11:15:32 +0200

    Création de la range; du remplacement des multiple

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,3 +1,8 @@
 class FizzBuzz:
     def affiche ():
-        pass
+        output = ""
+        for i in range(1,101):
+            if i % 3 == 0:
+                output += "Fizz"
+            if i % 5 == 0:
+    pass
